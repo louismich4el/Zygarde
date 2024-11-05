34ba009ad0fff1f31251abfe21b1e230c152d0df
louismich4el <141543114+louismich4el@users.noreply.github.com>
2024-11-05 13:49:29 +0700

    Delete Protected zygarde.lua

--- Protected zygarde.lua
+++
@@ -1,9 +0,0 @@
---// Zygarde
---// Version: 1
---// Owner: ItsLouisPlayz AKA 1ml3g1t
---// Developers: Paws
---// Average Update Per Week: 1
---// Obfuscated: Yes
---// Please Do Not Use This Loadstring Or Else You Wont Receive Updates Automatically.
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/louismich4el/Zygarde/main/ZygardeV1.txt", true))()
