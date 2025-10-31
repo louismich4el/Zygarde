ca4690e279302246757b61e4bc607517cc6066c7
1ml3g1t <141543114+louismich4el@users.noreply.github.com>
2025-10-31 13:34:28 +0700

    Add files via upload

---
+++ ZYG.lua
@@ -0,0 +1,11 @@
+-- This will only be up when zygarde if offline..
+
+
+-- ZO Samurai (Supports Only : Delta, KRNL, Volcano, Zenith, Wave)
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/90f717c8f2858ed3e3feb2674aa86469.lua"))()
+
+-- Combat Warriors
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/75bb2b71fc6c51bcc92c77290a2537a7.lua"))()
+
+-- The Strongest Battlegrounds
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/720ca401a8b75224254b35e0df05649e.lua"))()
