1adf9d5aec1aed180bdefffdeb7eecd085c2d3bb
1ml3g1t <141543114+louismich4el@users.noreply.github.com>
2026-02-20 10:47:34 +0700

    ZO Samurai.lua

--- ZO Samurai.lua
+++ ZO Samurai.lua
@@ -1,1 +1,6 @@
 loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/90f717c8f2858ed3e3feb2674aa86469.lua"))()
+
+if string.find(identifyexecutor():lower(), "xeno") then
+  game.Players.LocalPlayer:kick("Combat Warriors Zygarde isnt supported on Xeno, Please use another executor and make sure to Join our discord server for more info!\n(Invite link copied)")
+end
+
