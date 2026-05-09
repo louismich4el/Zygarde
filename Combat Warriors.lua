80db4298131093025cd3d4d13d0c2ca09b72059a
1ml3g1t <141543114+louismich4el@users.noreply.github.com>
2026-05-09 13:47:26 +0700

    Delete Combat Warriors.lua

--- Combat Warriors.lua
+++
@@ -1,3 +0,0 @@
-setclipboard("https://discord.gg/zygarde-new-era-1226331987661422646")
-game.Players.LocalPlayer:kick("Zygarde Has An New Loadstring Please Join Our Server! \n(Invite link copied)")
-
